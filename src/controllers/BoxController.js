160dc52482605b7847ec54da8983786ce9d9fd64
Romulo Rocha <romulorocha063@gmail.com>
2019-04-15 00:45:08 -0300

    Padronização de código

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -1,19 +1,19 @@
 const Box = require('../models/Box');
 
 class BoxController {
-    async store(req, res) {
+  async store(req, res) {
+    this.box = await Box.create({ title: req.body.title });
 
-        const box = await Box.create({ title: req.body.title })
+    return res.json(this.box);
+  }
 
-        return res.json(box);
-    }
-    async show(req, res) {
-        const box = await Box.findById(req.params.id).populate({
-            path: 'files',
-            options: { sort: { createdAt: -1 } }
-        })
-        return res.json(box)
-    }
+  async show(req, res) {
+    this.box = await Box.findById(req.params.id).populate({
+      path: 'files',
+      options: { sort: { createdAt: -1 } },
+    });
+    return res.json(this.box);
+  }
 }
 
 module.exports = new BoxController();
--- src/models/Box.js
+++ src/models/Box.js
@@ -2,20 +2,19 @@ const mongoose = require('mongoose');
 
 
 const Box = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    files: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'File'
-        }
-    ]
-},
+  title: {
+    type: String,
+    required: true,
+  },
+  files: [
     {
-        timestamps: true
-    }
-);
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'File',
+    },
+  ],
+},
+{
+  timestamps: true,
+});
 
-module.exports = mongoose.model('Box', Box)
+module.exports = mongoose.model('Box', Box);
